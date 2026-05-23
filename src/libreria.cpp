e160b8d8b9ee14e7a382603274500848d44c617e
CALCAGNI LUCA <calcagni.luca@dominio.local>
2026-05-23 11:39:58 +0200

    fixata la funzione media che non stampa la media corretta

--- src/libreria.cpp
+++ src/libreria.cpp
@@ -11,11 +11,15 @@
  * Calcola la media in km delle varie tratte
  */
 void tratta_media(Viaggio_t &x) {
+    int contatore = 0;
+    float somma = 0;
     for (int i = 0; i < N_FERMATE_STOP; i++) {
-        x.media += x.tratte[i];
+        if (x.tratte[i] > 0) {
+            contatore += 1;
+            somma += x.tratte[i];
+        }
     }
-    x.media /= x.numero_tratte;
-    x.numero_tratte++;
+    x.media = somma / contatore;
 }
 
 /**
@@ -25,8 +29,8 @@ bool aggiungi_tratta(Viaggio_t &x, float km) {
     if (x.numero_tratte > N_FERMATE_STOP) {
         return false;
     }
-    x.tratte[x.numero_tratte - 2] = km;
     x.numero_tratte++;
+    x.tratte[x.numero_tratte-1] = km;
     return true;
 }
 
@@ -34,7 +38,6 @@ bool aggiungi_tratta(Viaggio_t &x, float km) {
  * Inizializza un pilota, dandogli una partenza p e una destinazione d
  */
 void crea_viaggio(Viaggio_t &x, string p, string d) {
-    x.numero_tratte++;
     x.partenza = p;
     x.destinazione = d;
 }
@@ -48,7 +51,9 @@ void stampa_viaggio(Viaggio_t &x) {
     cout << "destinazione: " << x.destinazione << endl;
     cout << "chilometri tratte: ";
     for (int i = 0; i < N_FERMATE_STOP; i++) {
-        cout << x.tratte[i] << " ";
+        if (x.tratte[i] > 0) {
+            cout << x.tratte[i] << " ";
+        }
     }
     cout << endl;
     cout << "numero di tratte fino ad ora: " << x.numero_tratte << endl;
