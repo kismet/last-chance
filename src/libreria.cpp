a871e91c9cc471ad45090411532a723bc7e1a5cf
PANDOLFI MARCO <pandolfi.marco@dominio.local>
2026-05-23 10:42:33 +0200

    create the functions in libreria.cpp

--- src/libreria.cpp
+++ src/libreria.cpp
@@ -1,39 +1,78 @@
-/**
-* Calcola la media di punteggi
- */
-void calcola_media(Pilota_t &x);
+//
+// Created by stefy on 22/05/2026.
+//
 
-/**
- * Aggiunge il p punti al pilota x, se ci sono posti liberi e altrimenti restituisce false
- */
-bool aggiungi_granpremio(Pilota_t &x, int p);
+#ifndef LAST_CHANCE_LIBRERIA_H
+#include "../inc/libreria_tipi.h"
+#include "../inc/libreria.h"
 
-/**
- * Inizializza un pilota, dandogli un nome n e una scuderia s
- */
-void crea_pilota(Pilota_t &x, string n, string s);
+#include "../inc/libreria_tipi.h"
 
-/**
- * Stampa tutte le informazioni del pilota x: nome, cognome, numero di gran premi e punteggio per ogni gran premio
- * e infine la media
- */
-void stampa_pilota(Pilota_t *x);
+void costo_medio_libri(Libri_t &x) {
+    if (x.num_libri == 0) {
+        x.media=0.0;
+    }
+}
 
-/**
- * Togli p punti dall'ultimo piazzamento del pilota x
- * OPZIONALE: per chi ha diritto alla riduzione
- */
-void retrocedi(Pilota_t *s, float punti);
+bool aggiungi_costo_libro(Libri_t &x, float p) {
+    if (x.num_libri<N_MATERIE) {
+        x.costo[x.num_libri]=p;
+        x.num_libri++;
+        return true;
+    }
+}
 
-/**
- * Restituisce il Pilota campione del mondo (quello con più punti)
- * OPZIONALE: per chi ha diritto alla riduzione
- */
-Pilota_t* campione_del_mondo(Pilota_t pilots[], int dim);
+void crea_lista_libri(Libri_t &x, string c, string i) {
+    x.anno=c;
+    x.indirizzo=i;
+    x.num_libri++;
+    x.media=0.0;
+}
 
-/**
- * Restituisce il nome della scuderia che ha fatto più punti
- * BONUS: per chi ha diritto alla riduzione
- */
-string scuderia_campione(Pilota_t pilots[], int dim);
+void stampa_lista_libri(Libri_t *x) {
+    if (x==nullptr) {
+        return;
+    }
+}
 
+void sconto(Libri_t *s, float punti) {
+    if (s!=nullptr and s->num_libri>0) {
+        s->costo[s->num_libri-1]=punti;
+
+        if (s->costo[s->num_libri-1]<0) {
+            s->costo[s->num_libri-1]=0;
+        }
+    }
+}
+
+Libri_t* libro_economico(Libri_t elenchi[], int dim) {
+    if (dim<=0) {
+        return nullptr;
+    }
+    Libri_t* lista_col_minimo=nullptr;
+    float minimo=-1.0;
+    bool trovato=false;
+
+    for (int i=0;i<dim;i++) {
+        for (int j=0; j<elenchi[i].num_libri; j++) {
+            if (!trovato or elenchi[i].costo[j]<minimo) {
+                minimo=elenchi[i].costo[j];
+                lista_col_minimo=&elenchi[i];
+                trovato=true;
+            }
+        }
+    }
+
+    return lista_col_minimo;
+}
+
+string scuderia_campione(Libri_t elenchi[], int dim) {
+    if (dim<=0) {
+        return "";
+    }
+}
+
+
+#define LAST_CHANCE_LIBRERIA_H
+
+#endif //LAST_CHANCE_LIBRERIA_H
