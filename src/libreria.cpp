c91deb62cdfc3766767018a80cad885ace3d6e35
MANNUCCI MARCO <mannucci.marco@dominio.local>
2026-05-23 10:45:01 +0200

    Implementata crea_gara e stampa_gara

--- src/libreria.cpp
+++ src/libreria.cpp
@@ -33,5 +33,36 @@ bool aggiungi_tappa(Gara_Tappe_t &x, float p) {
     x.tappe[x.num_tappe] = p;
     x.num_tappe++;
 
+    // Aggiorno anche la lunghezza media
+    lunghezza_media(x);
+
     return true;
+}
+
+// Inizializza una gara a tappe di nome n ed edizione e
+void crea_gara(Gara_Tappe_t &x, string n, int e) {
+
+    x.nome = n;
+    x.anno = e;
+    x.num_tappe = 0;
+    x.lunghezza_media_tappe = 0;
+
+    for ( int i = 0; i < N_MASSIMO_TAPPE; i++ ) {
+        x.tappe[i] = 0;
+    }
+
+}
+
+// Stampa tutte le informazioni della gara a tappe x: nome, edizione, numero di tappe,
+// lunghezza di ogni tappa, e infine la media
+void stampa_gara(Gara_Tappe_t *x) {
+
+    cout << "Nome: " << x->nome << endl;
+    cout << "Edizione: " << x->anno << endl;
+    cout << "Numero tappe: " << x->num_tappe << endl;
+    for ( int i = 0; i < x->num_tappe; i++ ) {
+        cout << "Tappa " << i + 1 << ": " << x->tappe[i] << endl;
+    }
+    cout << "Media: " << x->lunghezza_media_tappe << endl;
+
 }
