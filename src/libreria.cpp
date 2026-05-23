7f7b583a3757b4c8327b96bbc99d024d476f0c2a
MANNUCCI MARCO <mannucci.marco@dominio.local>
2026-05-23 10:33:42 +0200

    Implementata lunhezza_media

--- src/libreria.cpp
+++ src/libreria.cpp
@@ -5,3 +5,20 @@
 
 using namespace std;
 
+// Calcola la media di km di compiere in questa gara
+void lunghezza_media(Gara_Tappe_t &x) {
+
+    // Se le tappe sono 0 lunghezza media tappe = 0
+    if ( x.num_tappe == 0 ) {
+        x.lunghezza_media_tappe = 0;
+    }
+    else {
+        float sommaKm = 0;
+
+        for ( int i = 0; i < x.num_tappe; i++ ) {
+            sommaKm = sommaKm + x.tappe[i];
+        }
+
+        x.lunghezza_media_tappe = sommaKm / x.num_tappe;
+    }
+}
